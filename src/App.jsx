957a08e1b77288edb700ba41b215ca32a4dc5d9e
Bruno <121259934+amatbruno@users.noreply.github.com>
2024-07-22 21:43:38 +0200

    first styles & form interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import TaskPostIt from './components/TaskPostIt'
+import { IoMdClose } from "react-icons/io";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const togglePopup = () => {
+    const formEl = document.getElementById('popup-form');
+    formEl.classList.toggle('show');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header className='mt-10 text-center'>
+        <h1 className='text-6xl font-bold mb-10'>My Todo App</h1>
+        <p className='text-xl font-normal'>Manage all your tasks efficiently for your daily routine!</p>
+      </header>
+      <div className='flex justify-center py-10'>
+        <button
+          onClick={togglePopup}
+          className='border-4 text-xl py-1 px-5 rounded-lg font-medium hover:bg-white hover:text-black transition-all'>+ Add a new task
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <article id='popup-form' className='overlay-form'>
+          <div className="popup-box">
+            <div className='flex justify-end'>
+              <IoMdClose
+                onClick={togglePopup}
+                className='hover:cursor-pointer'
+                size='20'
+                color='black'
+              />
+            </div>
+            <form className="form-container">
+              <label
+                className="form-label"
+                htmlFor="name">
+                Title
+              </label>
+              <input
+                className="form-input"
+                type="text"
+                id="name"
+                name="name"
+                required />
+              <label
+                className="form-label"
+                htmlFor="email">
+                Description
+              </label>
+              <textarea
+                className="form-input resize-none"
+                id="email"
+                rows={2}
+                required >
+              </textarea>
+              <label
+                className="form-label"
+                htmlFor="state">
+                State
+              </label>
+              <select
+                required
+                className='form-input'
+                name="state"
+                id="state">
+                <option value="toBegin">To begin</option>
+                <option value="inProgress">In progress</option>
+                <option value="done">Done</option>
+              </select>
+              <button
+                className="btn-submit"
+                type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        </article>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='border mt-8 w-1/2 m-auto h-fit'>
+
+      </main>
+
     </>
   )
 }
---
+++ src/components/TaskPostIt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskPostIt() {
+    return (
+        <div>TaskPostIt</div>
+    )
+}
+
+export default TaskPostIt
